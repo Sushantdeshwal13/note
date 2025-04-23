d1eb0401fde367039c05cb53a9d2d35f39db9404
sushant <sushantdeshwal13@gmail.com>
2025-04-23 11:19:31 +0530

    changes

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
-const express = require("express");
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const app = express();
+const usermodel = require("./models/user");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
-const userModel = require('./usermodel');
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
-app.get("/", (req, res)=>{
-    res.send("hello");
+app.get('/',(req,res)=>{
+    res.render("index");
+});
+app.post('/create', (req,res)=>{
+    let {username, email, password, age} = req.body;
+
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt,  async(err, hash)=> {
+            let createduser = await usermodel.create({
+                username,
+                email,
+                password: hash,
+                age
+            })
+            let token = jwt.sign({email:"sush@gmail.com"},"shhhh");
+            res.cookie("token", token);
+            res.send(createduser);
+        });
+    });
 });
 
-app.get("/create", async(req, res)=>{
-    const userdata = await userModel.create({
-        name:"sushant",
-        username:"deshwal",
-        email:"sush@gmail.com"
-    })
-    res.send(userdata);
+app.get('/login',(req,res)=>{
+   res.render('login');
+});
+app.post('/login', async(req, res)=>{
+   let user =  await usermodel.findOne({email: req.body.email});
+   if(!user) return res.send("sth is wrong");
+
+   bcrypt.compare(req.body.password, user.password, function(err, result){
+    if(result){
+        let token = jwt.sign({email:user.email}, "shhhh");
+        res.cookie("token", token);
+        res.send("yes you can login");
+    }
+    else res.send("something is wrong");
+   })
+});
+
+
+app.get("/logout", function(req,res){
+    res.cookie("token","");
+    res.redirect("/");
 })
-app.get("/update", async(req, res)=>{
-      let updatedata = await userModel.findOneAndUpdate(
-        {name:"sushant"}, {name:"deshwal"}, {new:true}
-      )
-    
-    res.send(updatedata);
-})
-app.get("/read", async (req, res)=>{
-    let readuser = await userModel.find({username:"deshwal"});
-    res.send(readuser);
-})
-app.get("/delete", async (req,res)=>{
-    let deleteuser = await userModel.findOneAndDelete({username:"deshwal"});
-    res.send(deleteuser);
-})
+
 app.listen(3000);
